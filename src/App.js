60daa0eb03a1adad09b07d708d2827ce69574216
Ankit Shrivastava <ankit.sim1693@gmail.com>
2022-02-24 22:16:22 +0530

    React-Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Consulting from "./pages/Consulting";
+import ContactUs from "./pages/ContactUs";
+import Marketing from "./pages/Marketing";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/consulting" element={<Consulting />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/marketing" element={<Marketing />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Dropdown.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import "./Dropdown.css";
+import { Link } from "react-router-dom";
+
+const Dropdown = () => {
+  const [click, setClick] = useState(false);
+
+  const clickHandler = () => {
+    setClick((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <ul
+        onClick={clickHandler}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link
+                to={item.path}
+                className={item.cName}
+                onClick={() => setClick(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Dropdown;
